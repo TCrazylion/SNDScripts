0906e355178af7e6b1904a12e87bff80314ce9e5
TangRunzhe <tang_runzhe@outlook.com>
2025-07-16 08:30:58 +0800

    Update Comsic_A2Fishing.lua

--- Comsic_A2Fishing.lua
+++ Comsic_A2Fishing.lua
@@ -1,51 +1,67 @@
--- 开始前先领取第一个任务
+-- 开始前先领取第一个任务, 确认各插件配置
 -- /ice only 508
 comsic = 1
+debug = true -- 调试模式
+craft_time = 50 -- 50秒锻造时间
 
 while comsic == 1 do 
-    item1 = Inventory.GetItemCount(48233) -- 48233 = 任务制作材料 宇宙货箱
-    item2 = Inventory.GetItemCount(45961) -- 45961 = 任务用钓饵 淡水万能机械臂
-
-    while item1 ~= 20 or item2 ~= 999 do
-        --yield("/echo Waiting for target quest")
-        yield("/wait 5")
+    -- 48233 = 任务制作材料 宇宙货箱
+    -- 45961 = 任务用钓饵 淡水万能机械臂
+    while Inventory.GetItemCount(48233) ~= 20 or Inventory.GetItemCount(45961) ~= 999 do
+		if debug == true then
+			yield("/echo 等待目标任务")
+		end
+        yield("/wait 3")
     end
 
 	-- Fish
-	yield("/equipjob ".."捕鱼人")
-	yield("/ahpreset A2联合")
+	--yield("/equipjob ".."捕鱼人")
+	--yield("/ahpreset A2联合")
 	yield("/wait 3")
 	yield("/ahstart")
-    -- 防止GP不够不抛竿导致跳过等待
-	yield("/wait 120")
-	
-	while Svc.Condition[1] == false do
-		--yield("/echo Still Fishing")
+
+	-- 45917 = 任务水产品 重无结构陨铁
+	while Inventory.GetItemCount(45917) < 14 do
+		if debug == true then
+			yield("/echo 任务水产品不足")
+		end
 		yield("/wait 3")
+		if Svc.Condition[1] == true then
+			if debug == true then
+				yield("/echo 开始钓鱼")
+			end
+			yield("/ahstart")
+		end
 	end
 
-    -- 45917 = 任务水产品 重无结构陨铁
+	while Svc.Condition[1] == false do
+		if debug == true then
+			yield("/echo 钓鱼状态中")
+		end
+		yield("/wait 3")
+	end
 	
-	yield("/echo Ending Fishing")
-	yield("/wait 5")
+	yield("/echo 钓鱼完成，开始制作")
+	yield("/wait 3")
 	
 	-- Craft
 	yield("/ice stop")
-	yield("/echo swapping to ---> ".."锻铁匠")
+	yield("/wait 1")
 	yield("/equipjob ".."锻铁匠")
 	yield("/wait 2")
 	yield("/send N")
 	yield("/wait 2")
 	yield("/artisan endurance start")
 	--银牌+宏时间(冗余)
-	yield("/wait 50")
-	yield("/wait 50")
+	yield("/wait "..craft_time)
+	yield("/wait "..craft_time)
+	yield("/artisan endurance stop")
 	yield("/send ESCAPE")
 	yield("/wait 3")
 	yield("/equipjob ".."捕鱼人")
-	yield("/wait 5")
+	yield("/wait 3")
 	yield("/ice start")
     -- 结束后重新开始
-	yield("/wait 5")
-
+	yield("/wait 3")
+	yield("/echo 任务完成，重新开始")
 end
